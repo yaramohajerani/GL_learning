3a23a568ef3ab721311a16cb82f060079b9cb1dc
Yara Mohajerani <ymohajer@uci.edu>
2020-05-12 02:58:55 -0400

    new script for testing already trained script, fix loop and pinning point issue, and combine dates with all metadata

--- combine_dates.py
+++ combine_dates.py
@@ -12,85 +12,127 @@ import sys
 import getopt
 import numpy as np
 import fiona
+from shapely.geometry import LineString, mapping
 
 base_dir = os.path.expanduser('~/GL_learning_data/geocoded_v1')
+
+#-- make list of properties
+props = ['Track','D1','D2','D3','D4','O1','O2','O3','O4','T1','T2','X','Y']
+
+#-- get meta data from file name
+def get_info(fname):
+	a = re.split('_|-',fname)
+	dic = {}
+	for i,p in enumerate(props):
+		dic[p] = a[i+2]
+	return dic
+
 #-- main function
 def main():
 	#-- Read the system arguments listed after the program
-	long_options=['DIR=']
-	optlist,arglist = getopt.getopt(sys.argv[1:],'D:',long_options)
+	long_options=['DIR=','FILTER=']
+	optlist,arglist = getopt.getopt(sys.argv[1:],'D:F:',long_options)
 
 	#-- Set default settings
 	subdir = 'atrous_32init_drop0.2_customLossR727.dir'
+	filter = 0.
+	flt_str = ''
 	for opt, arg in optlist:
 		if opt in ("-D","--DIR"):
 			subdir = arg
+		elif opt in ("-F","--FILTER"):
+			if arg not in ['NONE','none','None','N','n',0]:
+				filter = float(arg)
+				flt_str = '_%.1fkm'%(filter/1000)
 
 	#-- make full directory paths
-	indir = os.path.join(base_dir,'Test_predictions.dir',subdir,'shapefiles.dir')
-	outdir = os.path.join(base_dir,'Test_predictions.dir',subdir,'combinedDates.dir')
+	indir = {}
+	for t in ['Train','Test']:
+		indir[t] = os.path.join(base_dir,'%s_predictions.dir'%t,subdir,'shapefiles.dir')
+	out_subdir = os.path.join(base_dir,'CombinedDates.dir',subdir)
+	#-- make directories if they don't exist
+	if not os.path.exists(out_subdir):
+		os.mkdir(out_subdir)
+	outdir = os.path.join(out_subdir,'combined_shapefiles%s.dir'%flt_str)
 	#-- make directories if they don't exist
 	if not os.path.exists(outdir):
 		os.mkdir(outdir)
-
+	
 	#-- get list of files
-	fileList = os.listdir(indir)
-	flist = [f for f in fileList if (f.endswith('.shp') and f.startswith('post'))]
+	flist = {}
+	for t in ['Train','Test']:
+		fileList = os.listdir(indir[t])
+		flist[t] = [f for f in fileList if (f.endswith('.shp') and f.startswith('post'))]
 
-	#-- extract dates from filenames
-	regex = re.compile('\d+')
-	dates = [None]*len(flist)
-	for i,f in enumerate(flist):
-		nums = re.findall(regex,f)
-		#-- objects are strings. Get the combination as 1 long number
-		dates[i] = int(nums[1]+nums[2]+nums[3]+nums[4])
-	dates = np.array(dates)
+	#-- Match the file labels
+	#-- everything except the last part (x,y,DIR) has to match
+	dates = {}
+	for t in ['Train','Test']:
+		dates[t] = [None]*len(flist[t])
+		for i,f in enumerate(flist[t]):
+			dates[t][i] = re.split('gl_|_x',f)[1]
+		dates[t] = np.array(dates[t],dtype='str')
 
 	#-- get number of unique dates
-	ud = np.unique(dates)
-	print(len(dates),len(ud))
+	ud = np.unique(np.concatenate((dates['Train'],dates['Test'])))
+	print(len(ud))
+	
+	#-- schema for writing files
+	out_schema = {'geometry': 'LineString','properties': {'ID':'str',\
+		'Track':'int','D1':'int','D2':'int','D3':'int','D4':'int',\
+		'O1':'int','O2':'int','O3':'int','O4':'int','T1':'str',\
+		'T2':'str','X':'str','Y':'str','Type':'str'}}
 
 	#-- go through dates and read and combine all corresponding files
 	for u in ud:
-		#-- make output label for given date
-		s = str(u)
-		date_lbl = '%s-%s-%s-%s'%(s[:6],s[6:12],s[12:18],s[18:24])
-		#-- get indices of all matching dates
-		ii, = np.nonzero(dates==u)
-		for i in ii:
-			#-- read shapefile flist[i]
-			src = fiona.open(os.path.join(indir,flist[i]))
-			meta = src.meta
-			#-- counters
-			c1,c2,c3 = 0,0,0
-			#-- open output file for centerlines and errors
-			with fiona.open(os.path.join(outdir,'centerline_%s.shp'%date_lbl),\
-				'w', **meta) as dst1:
-				with fiona.open(os.path.join(outdir,'errors_%s.shp'%date_lbl),\
-					'w', **meta) as dst2:
-					with fiona.open(os.path.join(outdir,'handdrawn_%s.shp'%date_lbl),\
-						'w', **meta) as dst3:
-						for j in range(len(src)):
-							g = next(src)
-							if g['properties']['ID'].startswith('cntr'):
-								#-- change ID to file name
-								g['properties']['ID'] = flist[i][flist[i].find('T'):-4]+'_%i'%c1
-								#-- write center line to file
-								dst1.write(g)
-								c1 += 1
-							elif (not g['properties']['ID'].startswith('lbl')):
-								#-- change ID to file name
-								g['properties']['ID'] = flist[i][flist[i].find('T'):-4]+'_%i'%c2
-								#-- write center line to file
-								dst2.write(g)
-								c2 += 1
-							else:
-								#-- change ID to file name
-								g['properties']['ID'] = flist[i][flist[i].find('T'):-4]+'_%i'%c3
+		for t in ['Train','Test']:
+			#-- get indices of all matching dates
+			ii, = np.nonzero(dates[t]==u)
+			for i in ii:
+				#-- parse aquisition info from file name
+				info = get_info(flist[t][i])
+				#-- read shapefile flist[i]
+				src = fiona.open(os.path.join(indir[t],flist[t][i]),'r')
+				meta = src.meta
+				#-- update schema
+				meta['schema'] = out_schema
+				#-- counters
+				c1,c2,c3 = 0,0,0
+				#-- open output file for centerlines and errors
+				dst1 =  fiona.open(os.path.join(outdir,'centerline_%s%s.shp'%(u,flt_str)),'w', **meta)
+				dst2 = fiona.open(os.path.join(outdir,'errors_%s%s.shp'%(u,flt_str)),'w', **meta)
+				if filter == 0:
+					dst3 = fiona.open(os.path.join(outdir,'handdrawn_%s.shp'%u),'w', **meta)
+				for j in range(len(src)):
+					g = next(src)
+					#-- add filter
+					if (len(g['geometry']['coordinates']) > 1) and \
+						(LineString(g['geometry']['coordinates']).length > filter):
+						#-- add properties
+						g['properties']['Type'] = t
+						for p in props:
+							g['properties'][p] = info[p]
+						if g['properties']['ID'].startswith('cntr'):
+							g['properties']['ID'] = '%s_%i'%(info['Track'],c1)
+							#-- write center line to file
+							dst1.write(g)
+							c1 += 1
+						elif (not g['properties']['ID'].startswith('lbl')):
+							g['properties']['ID'] = '%s_%i'%(info['Track'],c2)
+							#-- write center line to file
+							dst2.write(g)
+							c2 += 1
+						else:
+							if filter == 0:
+								g['properties']['ID'] = '%s_%i'%(info['Track'],c3)
 								#-- write center line to file
 								dst3.write(g)
 								c3 += 1
-			src.close()
+				if filter == 0:
+					dst3.close()
+				dst2.close()
+				dst1.close()
+				src.close()
 
 #-- run main program
 if __name__ == '__main__':
--- convert_shapefile.py
+++ convert_shapefile.py
@@ -15,6 +15,8 @@ import skimage
 import getopt
 import shapefile
 import scipy.ndimage as ndimage
+from collections import Counter
+from shapely.geometry import Polygon
 from skimage.graph import route_through_array
 from skimage.morphology import thin, skeletonize
 
@@ -41,15 +43,18 @@ def main():
 	lbl_list = [f for f in fileList if (f.endswith('.tif') and f.startswith('delineation'))]
 	
 	#-- Get list of prediction files
-	pred_dir = os.path.join(gdrive_out,'Test_predictions.dir',subdir)
-	fileList = os.listdir(pred_dir)
-	pred_list = [f for f in fileList if (f.endswith('.png') and f.startswith('pred'))]
-
-	#-- output directory
-	output_dir = os.path.join(outdir,'Test_predictions.dir',subdir,'shapefiles.dir')
-	#-- make directories if they don't exist
-	if not os.path.exists(output_dir):
-		os.mkdir(output_dir)
+	pred_list = {}
+	output_dir = {}
+	for t in ['Train','Test']:
+		pred_dir = os.path.join(gdrive_out,'%s_predictions.dir'%t,subdir)
+		fileList = os.listdir(pred_dir)
+		pred_list[t] = [f for f in fileList if (f.endswith('.png') and f.startswith('pred'))]
+	
+		#-- output directory
+		output_dir[t] = os.path.join(outdir,'%s_predictions.dir'%t,subdir,'shapefiles.dir')
+		#-- make directories if they don't exist
+		if not os.path.exists(output_dir[t]):
+			os.mkdir(output_dir[t])
 
 	#-- threshold for getting contours and centerlines
 	eps = 0.3
@@ -57,82 +62,154 @@ def main():
 	#-- loop through prediction files
 	#-- get contours and save each as a line in shapefile
 	#-- also save training label as line
-	for f in pred_list:
-		#-- read prediction file
-		im = imageio.imread(os.path.join(gdrive_out,'Test_predictions.dir',subdir,f)).astype(float)/255.
-		#-- Read corresponding label from tiff file
-		#-- first find the index of the corresponding file
-		file_ind = lbl_list.index(f.replace('pred','delineation').replace('.png','.tif'))
-		raster = rasterio.open(os.path.join(gdrive,'delineationtile_withoutnull_v1',lbl_list[file_ind]))
-		#-- get transformation matrix
-		trans = raster.transform
+	for t in ['Train','Test']:
+		for f in pred_list[t]:
+			#-- read prediction file
+			im = imageio.imread(os.path.join(gdrive_out,'%s_predictions.dir'%t,subdir,f)).astype(float)/255.
+			#-- Read corresponding label from tiff file
+			#-- first find the index of the corresponding file
+			file_ind = lbl_list.index(f.replace('pred','delineation').replace('.png','.tif'))
+			raster = rasterio.open(os.path.join(gdrive,'delineationtile_withoutnull_v1',lbl_list[file_ind]))
+			#-- get transformation matrix
+			trans = raster.transform
 
-		#-- read label line and divide to segments by getting contours
-		lbl = raster.read(1)
-		lbl_contours = skimage.measure.find_contours(lbl, 1.)
-		#-- initialize list of contour linestrings
-		lbl_cnts = [None]*len(lbl_contours)
-		#-- convert to coordinates
-		for n, contour in enumerate(lbl_contours):
+			#-- read label line and divide to segments by getting contours
+			lbl = raster.read(1)
+			lbl_contours = skimage.measure.find_contours(lbl, 1.)
+			#-- initialize list of contour linestrings
+			lbl_cnts = [None]*len(lbl_contours)
 			#-- convert to coordinates
-			x,y = rasterio.transform.xy(trans, contour[:,0], contour[:,1])
-			lbl_cnts[n] = [list(a) for a in zip(x,y)]
-		
-		#-- get contours of prediction
-		#-- close contour ends to make polygons
-		im[np.nonzero(im[:,0] > eps),0] = eps
-		im[np.nonzero(im[:,-1] > eps),-1] = eps
-		im[0,np.nonzero(im[0,:] > eps)] = eps
-		im[-1,np.nonzero(im[-1,:] > eps)] = eps
-		contours = skimage.measure.find_contours(im, eps)
-		#-- initialize list of contour linestrings
-		cnts = [None]*len(contours)
-		centers = [None]*len(contours)
-		#-- convert to coordinates
-		for n,contour in enumerate(contours):
+			for n, contour in enumerate(lbl_contours):
+				#-- convert to coordinates
+				x,y = rasterio.transform.xy(trans, contour[:,0], contour[:,1])
+				lbl_cnts[n] = [list(a) for a in zip(x,y)]
+			
+			#-- get contours of prediction
+			#-- close contour ends to make polygons
+			im[np.nonzero(im[:,0] > eps),0] = eps
+			im[np.nonzero(im[:,-1] > eps),-1] = eps
+			im[0,np.nonzero(im[0,:] > eps)] = eps
+			im[-1,np.nonzero(im[-1,:] > eps)] = eps
+			contours = skimage.measure.find_contours(im, eps)
+			#-- make contours into closed polyons to find pinning points
+			pols = [None]*len(contours)
+			for n,contour in enumerate(contours):
+				pols[n] = Polygon(zip(contour[:,0],contour[:,1]))
+			#-- intialize matrix of polygon containment
+			cmat = np.zeros((len(pols),len(pols)),dtype=bool)
+			#-- initialize list of loops
+			loops = []
+			for i in range(len(pols)):
+				for j in range(len(pols)):
+					if (i != j) and pols[i].contains(pols[j]):
+						#-- if the outer contour is significantly longer than the
+						#-- inner contour, then it's not a pinning point but a loop
+						#-- in the GL (use factor of 10 difference). In that case, get 
+						#-- the inner loop instead
+						if len(contours[i][:,0]) > 10*len(contours[j][:,0]):
+							cmat[j,i] = True
+							#-- the outer contour is a loop
+							loops.append(i)
+						else:
+							cmat[i,j] = True
+			#-- However, note that if one outer control has more than 1 inner contour,
+			#-- then it's not a pinning point and it's actually just noise.
+			#-- In that case, ignore the inner contours. We add a new array for 'noise' points
+			#-- to be ignored
+			noise = []
+			#-- get indices of rows with more than 1 True column in cmat
+			for i in range(len(cmat)):
+				if np.count_nonzero(cmat[i,:]) > 1:
+					noise_idx, = np.nonzero(cmat[i,:])
+					#-- concentante to noise list
+					noise += list(noise_idx)
+					#-- turn the matrix elements back off
+					for j in noise_idx:
+						cmat[i,j] = False 
+			#-- remove repeating elements
+			noise = list(set(noise))
+
+			#-- go through overlapping elements and get nonoverlapping area to convert to 'donuts'
+			#-- NOTE we will get the the contour corresponding to the inner ring
+			outer = []
+			inner = []
+			for i in range(len(pols)):
+				for j in range(len(pols)):
+					if cmat[i,j] and (i not in noise) and (j not in noise):
+						#-- save indices of inner and outer rings
+						outer.append(i)
+						if j not in loops:
+							inner.append(j)
+			#-- initialize list of contour linestrings
+			cnts = [None]*(len(contours)-len(noise))
+			centers = [None]*(len(contours)-len(outer)-len(noise))
+			#-- counters for centerlines and contours
+			cc = 0 # contouer counter
+			n = 0  # center line counter
 			#-- convert to coordinates
-			x,y = rasterio.transform.xy(trans, contour[:,0], contour[:,1])
-			cnts[n] = [list(a) for a in zip(x,y)]
-			#-- get centerlines
-			#-- initialize centerline plot
-			im2 = np.zeros(im.shape, dtype=int)
-			#-- draw line through contour
-			im2[np.round(contour[:, 0]).astype('int'), np.round(contour[:, 1]).astype('int')] = 1
-			im2 = thin(ndimage.binary_fill_holes(im2))
-			# xtmp,ytmp = np.nonzero(im2)
-			# startPoint = (int(xtmp[0]),int(ytmp[0]))
-			# endPoint = (int(xtmp[-1]),int(ytmp[-1]))
-			nl = len(contour[:,0])
-			startPoint = (int(round(contour[0,0])),int(round(contour[0,1])))
-			endPoint = (int(round(contour[int(nl/2),0])),int(round(contour[int(nl/2),1])))
-			inds, ws = route_through_array(1-im2, (startPoint[0], startPoint[1]),\
-				(endPoint[0], endPoint[1]), geometric=True,fully_connected=True)
-			#-- wrap up list of tuples
-			ii,jj = zip(*inds)
-			xc,yc = rasterio.transform.xy(trans, ii, jj)
-			centers[n] = [list(a) for a in zip(xc,yc)]
-		
-		#-- save all linestrings to file
-		outfile = os.path.join(output_dir,f.replace('pred','post').replace('.png','.shp'))
-		w = shapefile.Writer(outfile)
-		w.field('ID', 'C')
-		#-- loop over contours and write them
-		for n in range(len(cnts)):
-			w.line([cnts[n]])
-			w.record(n)
-		#-- loop over contour centerlines
-		for n in range(len(centers)):
-			w.line([centers[n]])
-			w.record('cntr%i'%n)
-		#-- loop over label contours and write them
-		for n in range(len(lbl_cnts)):
-			w.line([lbl_cnts[n]])
-			w.record('lbl%i'%n)
-		w.close()
-		# create the .prj file
-		prj = open(outfile.replace('.shp','.prj'), "w")
-		prj.write(raster.crs.to_wkt())
-		prj.close()
+			for idx,contour in enumerate(contours):
+				if idx not in noise:
+					#-- convert to coordinates
+					x,y = rasterio.transform.xy(trans, contour[:,0], contour[:,1])
+					cnts[cc] = [list(a) for a in zip(x,y)]
+					#-- get centerline onl if this is not an outer ring
+					if idx not in outer:
+						#-- if this is an inner ring, then the centerline is the same as the contour
+						if idx in inner:
+							centers[n] = cnts[cc].copy()
+						#-- if neither inner nor outer (i.e. not pinnin point), get route through thinned line
+						else:
+							#-- get centerlines
+							#-- initialize centerline plot
+							im2 = np.zeros(im.shape, dtype=int)
+							#-- draw line through contour
+							im2[np.round(contour[:, 0]).astype('int'), np.round(contour[:, 1]).astype('int')] = 1
+							im2 = thin(ndimage.binary_fill_holes(im2))
+							nl = len(contour[:,0])
+							startPoint = (int(round(contour[0,0])),int(round(contour[0,1])))
+							endPoint = (int(round(contour[int(nl/2),0])),int(round(contour[int(nl/2),1])))
+							inds, ws = route_through_array(1-im2, (startPoint[0], startPoint[1]),\
+								(endPoint[0], endPoint[1]), geometric=True,fully_connected=True)
+							#-- wrap up list of tuples
+							ii,jj = zip(*inds)
+							xc,yc = rasterio.transform.xy(trans, ii, jj)
+							try:
+								centers[n] = [list(a) for a in zip(xc,yc)]
+							except:
+								print(f)
+								print(inner)
+								print(outer)
+								print('n: %i, cc: %i'%(n,cc))
+								print(cmat)
+								print('len centers: %i, len outer: %i, len contours: %i, len noise: %i'%(
+									len(centers),len(outer),len(contours),len(noise)))
+								sys.exit('index out of bounds.')
+						#-- increment centerline counter
+						n += 1
+					#-- increment contour counter
+					cc += 1
+			
+			#-- save all linestrings to file
+			outfile = os.path.join(output_dir[t],f.replace('pred','post').replace('.png','.shp'))
+			w = shapefile.Writer(outfile)
+			w.field('ID', 'C')
+			#-- loop over contours and write them
+			for n in range(len(cnts)):
+				w.line([cnts[n]])
+				w.record('%i_%s'%(n,t))
+			#-- loop over contour centerlines
+			for n in range(len(centers)):
+				w.line([centers[n]])
+				w.record('cntr%i_%s'%(n,t))
+			#-- loop over label contours and write them
+			for n in range(len(lbl_cnts)):
+				w.line([lbl_cnts[n]])
+				w.record('lbl%i_%s'%(n,t))
+			w.close()
+			# create the .prj file
+			prj = open(outfile.replace('.shp','.prj'), "w")
+			prj.write(raster.crs.to_wkt())
+			prj.close()
 
 #-- run main program
 if __name__ == '__main__':
