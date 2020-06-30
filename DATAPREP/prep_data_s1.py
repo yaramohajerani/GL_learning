1358100edfd8fcb4b04a18e37f3e39b1f5da40cd
Seongsu Jeong <sjeong.kr@gmail.com>
2020-06-29 21:36:39 -0700

    prep_data_s1.py - code cleanup

--- DATAPREP/prep_data_s1.py
+++ DATAPREP/prep_data_s1.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import grup
-from fparam import geo_param
 import glob
 import grup
 import os
@@ -12,15 +10,20 @@ import sys
 
 if __name__=='__main__':
     str_usage='''
-    prep_data_s1.py -p /Users/seongsu/Desktop/ACCESS -dir 11 -nx 512 -ny 512 -ox 0 -oy 0
-    prep_data_s1.py -p /Users/seongsu/Desktop/ACCESS -c /u/oates-r0/eric/SENTINEL1 -p /Users/seongsu/Desktop/ACCESS -dir 11 -nx 512 -ny 512 -ox 0 -oy 0
-    prep_data_s1.py -p /Users/seongsu/Desktop/ACCESS -dir 01 -nx 512 -ny 512 -ox 0 -oy 0 #tiling in inversed x direction
-    prep_data_s1.py -p /Users/seongsu/Desktop/ACCESS -dir 10 -nx 512 -ny 512 -ox 0 -oy 0 #tiling in inversed y direction
-    prep_data_s1.py -p /Users/seongsu/Desktop/ACCESS -dir 11 -nx 512 -ny 512 -ox 256 -oy 256 #staggered tile
+    prep_data_s1.py -p /Users/username/ACCESS -dir 11 -nx 512 -ny 512 -ox 0 -oy 0
+    prep_data_s1.py -p /Users/username/ACCESS -c /u/oates-r0/eric/SENTINEL1 -dir 11 -nx 512 -ny 512 -ox 0 -oy 0
+    prep_data_s1.py -p /Users/username/ACCESS -dir 01 -nx 512 -ny 512 -ox 0 -oy 0 #tiling in inversed x direction
+    prep_data_s1.py -p /Users/username/ACCESS -dir 10 -nx 512 -ny 512 -ox 0 -oy 0 #tiling in inversed y direction
+    prep_data_s1.py -p /Users/username/ACCESS -dir 11 -nx 512 -ny 512 -ox 256 -oy 256 #staggered tile
+
+    Parameter description:
+    -p : Project directory, in which the subdirectory 'SHP' is located (with the shape file in it)
+    -c : Parent directory of coco data to find
+    -dir: Tiling direction
+    -ox, oy: Offset in x and y coordinates for staggered tiling
     '''
 
     #default setting
-    
     #Tiling
     nx_tile=512
     ny_tile=512
@@ -31,25 +34,17 @@ if __name__=='__main__':
     flag_overwrite_rasterized_shp=True
     dn_burn=255
 
-    if os.uname().sysname=='Darwin':
-        path_project='/Users/seongsu/Desktop/ACCESS'
-        path_shp='{}/SHP'.format(path_project)
-        path_rasterized_out='{}/SHP_RASTERIZED'.format(path_project)
-        path_coco_prefix='/Users/seongsu/u/oates-r0/eric/SENTINEL1'
-        path_tile_with_null='{}/TILED_WITH_NULL'.format(path_project)
-        path_tile_without_null='{}/TILED_WITHOUT_NULL'.format(path_project)
-
-    elif os.uname().sysname=='Linux':
-        path_project='/u/pennell-z1/eric/SEONGSU_SCRATCH/ACCESS'
-        path_shp='{}/SHP'.format(path_project)
-        path_rasterized_out='{}/SHP_RASTERIZED'.format(path_project)
-        path_coco_prefix='/u/oates-r0/eric/SENTINEL1'
-        path_tile_with_null='{}/TILED_WITH_NULL'.format(path_project)
-        path_tile_without_null='{}/TILED_WITHOUT_NULL'.format(path_project)
-
     
+    path_project=''
+    path_coco_prefix=''
+
+    path_shp='{}/SHP'.format(path_project)
+    path_rasterized_out='{}/SHP_RASTERIZED'.format(path_project)
+    path_tile_with_null='{}/TILED_WITH_NULL'.format(path_project)
+    path_tile_without_null='{}/TILED_WITHOUT_NULL'.format(path_project)
 
     if len(sys.argv)==1:
+        #Print out the usage when there is no input parameters
         print(str_usage)
 
     else:
@@ -82,12 +77,17 @@ if __name__=='__main__':
             elif sys.argv[idoi]=='-oy':
                 offset_y=int(sys.argv[idoi+1])
                 idoi+=2
-            
-        #print out the parameters
-        print('               Project path:',path_project)
-        print('             Shapefile path:',path_shp)
-        print(' Output tile path (w/ null):',path_tile_with_null)
-        print('Output tile path (w/o null):',path_tile_without_null)
+        
+        #check if the input argument makes sense
+        if path_project=='' or path_coco_prefix=='':
+            print('ERROR: Either ptoject path or coco prefix was not provided. These are mandatory.')
+            exit(1)
+        else:
+            #print out the parameters
+            print('               Project path:',path_project)
+            print('             Shapefile path:',path_shp)
+            print(' Output tile path (w/ null):',path_tile_with_null)
+            print('Output tile path (w/o null):',path_tile_without_null)
 
         #Create the output path if not exist
         if not os.path.exists(path_rasterized_out):
@@ -118,8 +118,6 @@ if __name__=='__main__':
         for i,filename_shp in enumerate(list_shp):
             print(i+1,':',filename_shp)
 
-            #/Users/seongsu/Desktop/ACCESS/SHP/
-            
             seg_filename_shp=filename_shp.split('/')[-1].split('_')
             seg_dates=seg_filename_shp[2].split('-')
             seg_id=seg_filename_shp[3].split('-')
@@ -142,18 +140,18 @@ if __name__=='__main__':
             dt=int((date1.timestamp()-date0.timestamp())/86400+0.5)
             
             path_gl=form_path_pair.format(PARENT=path_coco_prefix,
-                                        YEAR=year_dir,
-                                        DT=dt,
-                                        ORBIT=str_track,
-                                        ID0=seg_id[0],
-                                        ID1=seg_id[1],
-                                        TIME0=str_time0)
+                                          YEAR=year_dir,
+                                          DT=dt,
+                                          ORBIT=str_track,
+                                          ID0=seg_id[0],
+                                          ID1=seg_id[1],
+                                          TIME0=str_time0)
             
             filename_coco=form_coco.format(ID0=seg_id[0],
-                                        ID1=seg_id[1],
-                                        ID2=seg_id[2],
-                                        ID3=seg_id[3],
-                                        TIME1=str_time1)
+                                           ID1=seg_id[1],
+                                           ID2=seg_id[2],
+                                           ID3=seg_id[3],
+                                           TIME1=str_time1)
 
             filename_pwr1=form_pwrbmp.format(ID0=seg_id[0])
 
@@ -163,19 +161,38 @@ if __name__=='__main__':
                 filename_tiff=filename_shp.replace('.shp','.tif')
                 #Load DEM_gc_par in path_gl
                 print('Loading DEM_gc_par')
-                dem_gc_par=geo_param()
-                dem_gc_par.load('{}/DEM_gc_par'.format(path_gl))
+                xmin=None
+                ymax=None
+                xposting=None
+                yposting=None
+                nx_src=None
+                ny_src=None
+                with open('{}/DEM_gc_par'.format(path_gl)) as fin:
+                    lines_in=fin.readlines()
+                    for line_in in lines_in:
+                        if 'width:' in line_in:
+                            nx_src=int(line_in.split()[1])
+                        elif 'nlines:' in line_in:
+                            ny_src=int(line_in.split()[1])
+                        elif 'corner_north:' in line_in:
+                            ymax=float(line_in.split()[1])
+                        elif 'corner_east:' in line_in:
+                            xmin=float(line_in.split()[1])
+                        elif 'post_north:' in line_in:
+                            yposting=float(line_in.split()[1])
+                        elif 'post_east:' in line_in:
+                            xposting=float(line_in.split()[1])
 
                 #Rasterize the shp file
                 print('Rasterizing shp file')
-                x0=dem_gc_par.xmin-dem_gc_par.xposting/2
-                x1=x0+dem_gc_par.xposting*dem_gc_par.npix
-                y1=dem_gc_par.ymax-dem_gc_par.yposting/2
-                y0=y1+dem_gc_par.yposting*dem_gc_par.nrec
+                x0=xmin-xposting/2
+                x1=x0+xposting*nx_src
+                y1=ymax-yposting/2
+                y0=y1+yposting*ny_src
                 
                 command_rasterize=form_command_rasterize.format(BURN=dn_burn,
-                                                                RX=dem_gc_par.xposting,
-                                                                RY=-float(dem_gc_par.yposting),
+                                                                RX=xposting,
+                                                                RY=-float(yposting),
                                                                 XMIN=x0,
                                                                 YMIN=y0,
                                                                 XMAX=x1,
@@ -195,21 +212,18 @@ if __name__=='__main__':
 
                 #load the original coco
                 print('Loading Original coco')
-                raster_coco=np.fromfile('{}/{}'.format(path_gl,filename_coco),dtype=np.complex64).byteswap().reshape(dem_gc_par.nrec,dem_gc_par.npix)
+                raster_coco=np.fromfile('{}/{}'.format(path_gl,filename_coco),dtype=np.complex64).byteswap().reshape(ny_src,nx_src)
 
                 #load the geocoded pwrbmp
                 print('Loading done')
 
                 #chop the image
-                #chop_img(raster_coco,nx_tile,ny_tile,-1,1)
                 grid_x=np.arange(offset_x,raster_coco.shape[1],nx_tile)
                 if not dirx:
-                    #grid_x=grid_x+raster_coco.shape[1]-grid_x[-1]
                     grid_x=np.flipud(np.arange(raster_coco.shape[1]-offset_x,0,-nx_tile))
                 
                 grid_y=np.arange(offset_y,raster_coco.shape[0],ny_tile)
                 if not diry:
-                    #grid_y=grid_y+raster_coco.shape[0]-grid_y[-1]
                     grid_y=np.flipud(np.arange(raster_coco.shape[0]-offset_y,0,-ny_tile))
 
                 print('nx={},ny={}'.format(raster_coco.shape[1],raster_coco.shape[0]))
@@ -220,13 +234,6 @@ if __name__=='__main__':
                         print('x:{}-{}, y:{}-{}'.format(grid_x[j],grid_x[j+1],grid_y[i],grid_y[i+1]))
                         #chop coco
                         #chop rasterized delineation
-                        path_tile_out='{PATH_TILE}/DIR{DIRX:d}{DIRY:d}'.format(PATH_TILE=path_tile_with_null,
-                                                                            DIRX=dirx,
-                                                                            DIRY=diry)
-                        #if not os.path.exists(path_tile_out):
-                        #    os.makedirs(path_tile_out)
-
-
                         filename_base=filename_shp.split('/')[-1].replace('.shp','')
                         filename_chopped_coco='{PATH_OUT}/coco_{BASE}_x{IDX:04d}_y{IDY:04d}_DIR{DIRX:d}{DIRY:d}.tif'.\
                                             format(PATH_OUT=path_tile_with_null,
@@ -244,11 +251,7 @@ if __name__=='__main__':
                                                     DIRX=dirx,
                                                     DIRY=diry)
 
-                        #print('COCO tile       : {}'.format(filename_chopped_coco))
-                        #print('DELINEATION tile: {}\n'.format(filename_chopped_delineation))
-
                         #Calculate the geotransform information
-                        #
                         coco_tile_out.z=raster_coco[grid_y[i]:grid_y[i+1],grid_x[j]:grid_x[j+1]]
                         delineation_tile_out.z=geotiff_delineation.z[grid_y[i]:grid_y[i+1],grid_x[j]:grid_x[j+1]]
                         gtf_list=[geotiff_delineation.GeoTransform[0]+grid_x[j]*geotiff_delineation.GeoTransform[1],
@@ -270,13 +273,5 @@ if __name__=='__main__':
                             delineation_tile_out.write(filename_chopped_delineation.replace('WITH','WITHOUT'))
             else:
                 print('COCO not found: {}/{}'.format(path_gl,filename_coco))
-
-
-            #gl_007_180629-180705-180705-180711_011583-022654-022654-011758_T050836_T050921.shp
-            #coco011583-022654-011758-022829_T050836.flat.topo_off.deramp.geo.psfilt
-            #coco011583-022654-011758-022829_T050901.flat.topo_off.deramp.geo.psfilt
-            #coco011583-022654-022654-011758_T050946.flat.topo_off.deramp.geo.psfilt
-            #coco011583-022654-022654-011758_T050857.flat.topo_off.deramp.geo.psfilt
-            #coco011583-022654-022654-011758_T050921.flat.topo_off.deramp.geo.psfilt
 
 
