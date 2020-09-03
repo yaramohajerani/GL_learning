b3124bae3c5b0aece599e1699cec7d76e44ac484
Seongsu Jeong <sjeong.kr@gmail.com>
2020-09-03 11:38:32 -0700

    prep_data.py - framework for csk class

--- DATAPREP/prep_data.py
+++ DATAPREP/prep_data.py
@@ -206,5 +206,94 @@ class sentinel1:
 
         return raster_out
 
+    
+class csk:
+    #NOTE: The implementation below is just the copy of the class 'sentinel1' above.
+    #      Use the following codes just only for the framework.
+    #TODO: Modity the codes below to suit into CSK
+
+    def __init__(self):
+        self.form_path_pair='{PARENT}/{YEAR}/{DT}d/Track{ORBIT}/Track{ID0}-{ID1}_T{TIME0}'
+        self.form_coco='coco{ID0}-{ID1}-{ID2}-{ID3}_T{TIME1}.flat.topo_off.deramp.geo.psfilt'
+        self.path_parent='/u/oates-r0/eric/SENTINEL1' #default root directory of S1 processing data in UCI
+
+    #example pair of coco and gltif
+    #                            gl_164_180919-181001-181001-181013_012790-012965-012965-013140_T233047_T233048.tif
+    #.../SENTINEL1/2018/12d/Track164/Track012790-012965_T233047/coco012790-012965-012965-013140_T233048.flat.topo_off.deramp.geo.psfilt
+    
+    #convert coco name into gl.tif name
+    def coco2gl(self,str_coco):
+        #TODO: Complete implementing this
+        print(str_coco,'IMPLEMENT ME!!!')
+        pass
+
+    #convert gl.tif name into coco name
+    def gl2coco(self,str_gl,path_root=None):
+
+        if path_root==None:
+            path_root=self.path_parent
+        
+        #parse str_gl
+        #gl_164_180919-181001-181001-181013_012790-012965-012965-013140_T233047_T233048.tif
+        #00 111 222222 222222 222222 222222 333333 333333 333333 333333 4444444 5555555
+        str_orbit=str_gl.split('_')[1]
+        seg_dates=str_gl.split('_')[2].split('-')
+        seg_id=str_gl.split('_')[3].split('-')
+        str_time0=str_gl.split('_')[4].replace('T','')
+        str_time1=str_gl.split('_')[5].replace('T','').replace('.tif','')
+
+        #detect the year string
+        str_year='2000'
+        for datestr in seg_dates:
+            if int(datestr[0:3])>int(str_year[3:]):
+                str_year='20{}'.format(datestr[0:2])
+    
+        #detect dt portion
+        t0=datetime.datetime.strptime(seg_dates[0],'%y%m%d')
+        t1=datetime.datetime.strptime(seg_dates[1],'%y%m%d')
+        dt=abs((t1-t0).days)
+
+        str_path_pair=self.form_path_pair.format(PARENT=self.path_parent,
+                                                 YEAR=str_year,
+                                                 DT=dt,
+                                                 ORBIT=str_orbit,
+                                                 ID0=seg_id[0],
+                                                 ID1=seg_id[1],
+                                                 TIME0=str_time0)
+
+        str_coco=self.form_coco.format(ID0=seg_id[0],
+                                       ID1=seg_id[1],
+                                       ID2=seg_id[2],
+                                       ID3=seg_id[3],
+                                       TIME1=str_time1)
+
+        return '{}/{}'.format(str_path_pair,str_coco)
+
+    def load_coco(self,filename_coco,backscatter=False):
+        #NOTE: Turn backscatter True when want to include backscatter
+        #TODO: Implement the case whehn backscatter=True
+
+        with open('{}/DEM_gc_par'.format(os.path.dirname(filename_coco)),'r') as fin:
+            lines_in=fin.readlines()
+        npix=0
+        nrec=0
+        for line in lines_in:
+            if 'width:' in line:
+                npix=int(line.split()[1])
+            if 'nlines:' in line:
+                nrec=int(line.split()[1])
+
+            if npix>0 and nrec>0:
+                break
+        
+        print('NPIX={}, NREC={}'.format(npix,nrec))
+
+        raster_out=grup.raster()
+        raster_out.nx=npix
+        raster_out.ny=nrec
+        raster_out.numbands=1
+        raster_out.z=np.fromfile(filename_coco,dtype=np.complex64).byteswap().reshape((nrec,npix))
+
+        return raster_out
 
 
