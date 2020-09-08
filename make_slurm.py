3173988fbadc081e511bd0ed717a46b9adea1011
Yara Mohajerani <ymohajer@uci.edu>
2020-09-08 15:47:58 -0400

    update slurm job for run_prediction to include username, and remove slow parititon

--- make_slurm.py
+++ make_slurm.py
@@ -12,8 +12,8 @@ import getopt
 #-- main function
 def main():
 	#-- Read the system arguments listed after the program
-	long_options=['DATA_DIR=','SLURM_DIR=','CODE_DIR=','NUM=','MODEL=','CLOBBER']
-	optlist,arglist = getopt.getopt(sys.argv[1:],'D:S:C:N:M:L',long_options)
+	long_options=['DATA_DIR=','SLURM_DIR=','CODE_DIR=','NUM=','MODEL=','USER=','CLOBBER']
+	optlist,arglist = getopt.getopt(sys.argv[1:],'D:S:C:N:M:U:L',long_options)
 
 	#-- Set default settings
 	ddir = '/DFS-L/DATA/gl_ml/SENTINEL1_2018/Track007'
@@ -22,6 +22,7 @@ def main():
 	num = 1500
 	CLOBBER = False
 	model_str = 'atrous_32init_drop0.2_customLossR727.dir'
+	user = 'ymohajer'
 	for opt, arg in optlist:
 		if opt in ("-D","--DATA_DIR"):
 			ddir = os.path.expanduser(arg)
@@ -33,6 +34,8 @@ def main():
 			num = int(arg)
 		elif opt in ("-M","--MODEL"):
 			model_str = arg
+		elif opt in ("-U","--USER"):
+			user = arg
 		elif opt in ("L","--CLOBBER"):
 			CLOBBER = True
 
@@ -64,9 +67,9 @@ def main():
 		fid.write("#SBATCH -n1\n")
 		fid.write("#SBATCH --mem=20G\n")
 		fid.write("#SBATCH -t0-06:00:00\n")
-		fid.write("#SBATCH -p sib2.9,nes2.8,has2.5,brd2.4,ilg2.3,m-c2.2,m-c1.9,m2090\n")
+		fid.write("#SBATCH -p sib2.9,nes2.8,has2.5,brd2.4,ilg2.3,m-c2.2,m2090\n")
 		fid.write("#SBATCH --job-name=%s_%i\n"%(os.path.basename(ddir),cc))
-		fid.write("#SBATCH --mail-user=ymohajer@uci.edu\n")
+		fid.write("#SBATCH --mail-user=%s@uci.edu\n"%user)
 		fid.write("#SBATCH --mail-type=FAIL\n\n")
 
 		fid.write('source ~/miniconda3/bin/activate gl_env\n')
