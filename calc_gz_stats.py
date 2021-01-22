15fbd909f3a4107f081bc7305a500d8225bdf2c4
Yara Mohajerani <ymohajer@uci.edu>
2021-01-22 03:57:41 -0500

    comparison stats between expected and delineated GZ widths

---
+++ calc_gz_stats.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+u"""
+calc_gz_stats.py
+by Yara Mohajerani
+
+Comparison stats of expected and delineated
+GZ widths
+
+Last Update 01/2021
+"""
+import os
+import pathlib
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+infile = os.path.join(pathlib.Path.home(),'GL_learning_data',\
+	'6d_results','GZ_widths-hybrid_Getz_comparison.csv')
+#-- read data
+df = pd.read_csv(infile)
+ml = np.array(df['ML_WIDTH(m)'])/1e3
+he = np.array(df['HE_WIDTH(m)'])/1e3
+ra = np.array(df['ML/HE ratio'])
+
+#-- remove non-valid elements
+# ii = np.where((he<0.1) | (he>8))
+ii = np.where((he<0.1) | (he>20))
+ml[ii] = np.nan
+he[ii] = np.nan
+ra[ii] = np.nan
+print(np.nanmin(ra),np.nanmax(ra))
+#-- make scatter plot
+fig = plt.figure(1,figsize=(8,5))
+sc = plt.scatter(ml,he,c=ra,s=20, vmin=0,vmax=100, cmap='viridis',alpha=0.7)
+cbar = plt.colorbar(sc)
+cbar.ax.get_yaxis().labelpad = 20
+cbar.ax.set_ylabel('ML:HE width ratio', rotation=270,fontweight='bold',fontsize=18)
+cbar.ax.tick_params(labelsize=14)
+#-- regression 
+x = sm.add_constant(ml)
+model = sm.OLS(he,x,missing='drop')
+fit = model.fit()
+print(fit.summary()) 
+#-- get trend and uncertainty
+tr = fit.params[1]
+er = fit.bse[1]
+#-- plot 45-degree line
+l = np.arange(np.nanmax(ml))
+# plt.plot(l,l,color='darkred',linestyle='--',linewidth=0.5,label=r"$x=y$ line")
+#-- plot trend line
+pred = fit.params[0] + l*tr
+plt.plot(l,pred,color='red',linestyle='-',linewidth=1.2,\
+	label=r"Fitted $HE:ML$ Ratio"+"\n"+r"%.1e $\pm$ %.1e"%(tr,er))
+plt.legend(fontsize=18)
+plt.xlabel("ML widths (km)",fontsize=18)
+plt.ylabel('HE widths (km)',fontsize=18)
+plt.tick_params(labelsize=14)
+plt.xticks([0,5,10,15,20])
+plt.yticks([0,5,10,15,20])
+plt.savefig(infile.replace('.csv','_distribution.pdf'),format='PDF')
+plt.close(fig)
